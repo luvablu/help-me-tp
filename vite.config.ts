d8f7cf91e541f59c98c3d69cd3d11f7772ae03c8
luvablu <146785616+luvablu@users.noreply.github.com>
2024-09-27 21:07:51 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/<REPO>/",
+  base: "/help-me-tp/",
   plugins: [react(), tsconfigPaths()],
 });
